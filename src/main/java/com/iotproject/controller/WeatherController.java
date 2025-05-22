8b6b449b1773985d52a55e59b04e18a110b45dc1
German Moraga Fontan <german.moraga.fontan.com>
2025-05-22 16:21:56 +0200

    API Works

---
+++ src/main/java/com/iotproject/controller/WeatherController.java
@@ -0,0 +1,22 @@
+package com.iotproject.controller;
+
+import com.iotproject.service.WeatherService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping
+    public Map<String, Object> getWeather(@RequestParam String city) {
+        return weatherService.getWeather(city);
+    }
+}
--- src/main/java/com/iotproject/service/WeatherService.java
+++ src/main/java/com/iotproject/service/WeatherService.java
@@ -1,28 +1,26 @@
 package com.iotproject.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+import java.util.Map;
 
 @Service
 public class WeatherService {
     
-    @Value("${weather.api.key}")
-    private String apiKey;
-    
-    @Value("${weather.api.url}")
-    private String apiUrl;
-    
-    private final RestTemplate restTemplate;
-    
-    public WeatherService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-    
-    @Scheduled(fixedRate = 3600000) // Run every hour
-    public void fetchWeatherData() {
-        // TODO: Implement weather API call
-        // This will be implemented based on the specific weather API you choose
+   private static final String API_KEY = "5893176a2f4144a3840140049252205"; 
+
+    public Map<String, Object> getWeather(String city) {
+        String url = UriComponentsBuilder
+                .fromUriString("http://api.weatherapi.com/v1/current.json")
+                .queryParam("key", API_KEY)
+                .queryParam("q", city)
+                .build().toString();
+
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+        return response;
     }
 } 
